1928e5aa38b538f684b4b68913c6f4b1b562bba3
Erikas Bulba <b.expire@gmail.com>
2017-05-29 17:13:15 +0300

    Update ContentController.php

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -1,77 +1,70 @@
-<?php 
-
-namespace ToDolist\Controllers;
-
-use Plenty\Plugin\Controller;
-use Plenty\Plugin\Http\Request;
-use Plenty\Plugin\Templates\Twig;
-use ToDoList\Contracts\ToDoRepositoryContract;
-use Plenty\Plugin\Log\Loggable;
-
-
-/**
- * Class ContentController
- * @package ToDoList\Controllers
- */
+<?php
  
- class ContentController extends Controller {
-     
+ namespace ToDoList\Controllers;
+ 
+ use Plenty\Plugin\Controller;
+ use Plenty\Plugin\Http\Request;
+ use Plenty\Plugin\Templates\Twig;
+ use ToDoList\Contracts\ToDoRepositoryContract;
+ use Plenty\Plugin\Log\Loggable;
+ 
+ /**
+  * Class ContentController
+  * @package ToDoList\Controllers
+  */
+ class ContentController extends Controller
+ {
      use Loggable;
-     
+ 
      /**
       * @param Twig                   $twig
       * @param ToDoRepositoryContract $toDoRepo
       * @return string
       */
-      public function showToDo(Twig $twig, ToDoRepositoryContract $toDoRepo): string {
+     public function showToDo(Twig $twig, ToDoRepositoryContract $toDoRepo): string
+     {
          $toDoList = $toDoRepo->getToDoList();
          $templateData = array("tasks" => $toDoList);
          return $twig->render('ToDoList::content.todo', $templateData);
-      }
-      
-        /**
-        * @param  \Plenty\Plugin\Http\Request $request
-        * @param ToDoRepositoryContract       $toDoRepo
-        * @return string
-        */
-        
-      public function createToDo(Request, $request, ToDoRepositoryContract $toDoRepo): string{
-          $newToDo = $toDoRepo->createTask(requrest->all());
-          
-          // add logging here
-          
-          $this
-          ->getLogger("contentController_createToDo")
-          ->setRefferenceType("ToDoId")
-          ->setRefferenceValue($newToDoId->id)
-          ->info("ToDoList.createToDoInformation",['userId' => $newToDo->userId ]);
-          
-          return json_encode($newToDo);
-          
-      }
-      
-      /**
-       * @param int $id
-       * @param ToDoRepositoryContract $toDoRepo
-       * @return string
-       */
-       
-       public function updateToDo(int $id, ToDoRepositoryContract $toDoRepo): string {
-           $updateToDo = $toDoRepo->updateTast($id);
-           return json_encode($updateToDo);
-       }
-       
-       
-       /**
-       * @param int $id
-       * @param ToDoRepositoryContract $toDoRepo
-       * @return string
-       */
-       
-       public function deleteToDo(int $id, ToDoRepositoryContract $toDoRepo): string {
-           $deleteToDo = $toDoRepo->deleteTask($id);
-           return json_encode($deleteToDo);
-       }
-     
- }
+     }
  
+     /**
+      * @param  \Plenty\Plugin\Http\Request $request
+      * @param ToDoRepositoryContract       $toDoRepo
+      * @return string
+      */
+     public function createToDo(Request $request, ToDoRepositoryContract $toDoRepo): string
+     {
+         $newToDo = $toDoRepo->createTask($request->all());
+ 
+         $this
+             ->getLogger('ContentController_createToDo')
+             ->setReferenceType('toDoId') // optional
+             ->setReferenceValue($newToDo->id) // optional
+             ->info('ToDoList::migration.createToDoInformation', ['userId' => $newToDo->userId ]);
+ 
+         return json_encode($newToDo);
+     }
+ 
+     /**
+      * @param int                    $id
+      * @param ToDoRepositoryContract $toDoRepo
+      * @return string
+      */
+     public function updateToDo(int $id, ToDoRepositoryContract $toDoRepo): string
+     {
+         $updateToDo = $toDoRepo->updateTask($id);
+         return json_encode($updateToDo);
+     }
+ 
+     /**
+      * @param int                    $id
+      * @param ToDoRepositoryContract $toDoRepo
+      * @return string
+      */
+     public function deleteToDo(int $id, ToDoRepositoryContract $toDoRepo): string
+     {
+         $deleteToDo = $toDoRepo->deleteTask($id);
+         return json_encode($deleteToDo);
+     }
+ }
