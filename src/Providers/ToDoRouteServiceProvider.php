b1b7dd04b3b3b515247d88ab2a9ffdcd4654136f
Erikas Bulba <b.expire@gmail.com>
2017-05-29 16:12:15 +0300

    Update ToDoRouteServiceProvider.php

--- src/Providers/ToDoRouteServiceProvider.php
+++ src/Providers/ToDoRouteServiceProvider.php
@@ -1,26 +1,25 @@
-<?php 
-
+<?php
+ 
 namespace ToDoList\Providers;
-
+ 
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\Router;
-
-
+ 
 /**
- * Class ToDoReouteServiceProvider
+ * Class ToDoRouteServiceProvider
  * @package ToDoList\Providers
  */
+class ToDoRouteServiceProvider extends RouteServiceProvider
+{
+    /**
+     * @param Router $router
+     */
+    public function map(Router $router)
+    {
+        $router->get('todo', 'ToDoList\Controllers\ContentController@showToDo');
+        $router->post('todo', 'ToDoList\Controllers\ContentController@createToDo');
+        $router->put('todo/{id}', 'ToDoList\Controllers\ContentController@updateToDo')->where('id', '\d+');
+        $router->delete('todo/{id}', 'ToDoList\Controllers\ContentController@deleteToDo')->where('id', '\d+');
+    }
  
- class ToDoRouteServiceProvider extends RouteServiceProvider {
-     
-     /**
-      *  @param Router $router
-      */
-      
-      public function map(Router $router){
-          $router->get('todo', 'ToDoList\Controllers\ContentController@showToDo');
-          $router->post('todo', 'ToDoList\Controllers\ContentController@createToDo');
-          $router->put('todo/{id}', 'ToDolist\Controllers\ContentController@updateToDO')->where('id', '\d+');
-          $router->delete('todo/{id}', 'ToDoList\Controllers\ContentController@deleteToDo')->where('id', '\d+');
-      }
- }
+}
